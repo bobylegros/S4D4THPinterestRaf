ca363f89cd5cc74081fe25df696d3556b45d9b2e
Rafael <rafael@rafael.lan>
2018-02-01 17:46:20 +0100

    Migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many pin
-  
+  has_many pins
+
 end
--- db/migrate/20180201163445_create_pins.rb
+++ db/migrate/20180201163445_create_pins.rb
@@ -2,7 +2,8 @@ class CreatePins < ActiveRecord::Migration[5.1]
   def change
     create_table :pins do |t|
       t.string :url
-
+      t.references :user, foreign_key: true
+      
       t.timestamps
     end
   end
